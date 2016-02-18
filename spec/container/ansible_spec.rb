e05c26146474a2328f748ea28962774f8f1352e9
FGtatsuro <fujiistorage@gmail.com>
2016-02-18 15:49:46 +0900

    Add specs for this role.

--- spec/container/ansible_spec.rb
+++ spec/container/ansible_spec.rb
@@ -3,3 +3,11 @@ require 'spec_helper_docker'
 describe package('python-dev'), :if => os[:family] == 'debian' do
   it { should be_installed }
 end
+
+describe package('build-essential'), :if => os[:family] == 'debian' do
+  it { should be_installed }
+end
+
+describe package('ansible') do
+  it { should be_installed.by('pip') }
+end
--- spec/localhost/ansible_spec.rb
+++ spec/localhost/ansible_spec.rb
@@ -3,3 +3,11 @@ require 'spec_helper'
 describe package('python-dev'), :if => os[:family] == 'debian' do
   it { should be_installed }
 end
+
+describe package('build-essential'), :if => os[:family] == 'debian' do
+  it { should be_installed }
+end
+
+describe package('ansible') do
+  it { should be_installed.by('pip') }
+end
