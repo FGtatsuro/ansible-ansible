81953224c10e086b73a0b1a22bc938bc63c52761
FGtatsuro <fujiistorage@gmail.com>
2016-02-21 22:01:36 +0900

    Add dependency for FGtatsuro.python-requirements.
    Some specs are removed because they are already checked in FGtatsuro.python-requirements.

--- spec/ansible_spec.rb
+++ spec/ansible_spec.rb
@@ -1,13 +1,5 @@
 require "spec_helper_#{ENV['SPEC_TARGET_BACKEND']}"
 
-describe package('python-dev'), :if => os[:family] == 'debian' do
-  it { should be_installed }
-end
-
-describe package('build-essential'), :if => os[:family] == 'debian' do
-  it { should be_installed }
-end
-
 describe package('ansible') do
   it { should be_installed.by('pip') }
 end
